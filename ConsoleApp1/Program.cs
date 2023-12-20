2702f70eb4025fdab5eb21196ac21155df6d8d6a
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2023-12-21 02:31:51 +0600

    'Учет личных финансов' with SQL

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Finanse;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,19 +17,13 @@ namespace PersonalFinanceTracker
         static void Main(string[] args)
         {
             StartProgramm();
-
         }
 
         public static void StartProgramm()
         {
+            string connectionString = @"Data Source=OLZHAS;Initial Catalog=Finanse;User id=sa;Password=123;";
 
-
-            Console.WriteLine("\tЗдравствуйте, хотите загрузить данные из файла? (Yes, либо что угодно если нет)");
-            string answer = Console.ReadLine();
-            if (answer.ToLower() == "yes")
-            {
-                Bank.LoadData();
-            }
+            UserInterface userInterface = new UserInterface(connectionString);
 
             bool exit = false;
             while (!exit)
@@ -38,34 +34,73 @@ namespace PersonalFinanceTracker
                 Console.WriteLine("2. Просмотреть историю транзакций");
                 Console.WriteLine("3. Показать общий баланс");
                 Console.WriteLine("4. Выход");
-                Console.WriteLine("5. Сохранить транзакции в файл (txt)");
-                Console.WriteLine("6. Очистить консоль");
+                Console.WriteLine("5 Очистить консоль");
                 int choice = int.Parse(Console.ReadLine());
 
                 switch (choice)
                 {
                     case 1:
-                        Bank.AddTransaction();
+                        Transaction newTransaction = NewTransaction();
+                        if(newTransaction.Category == "Error")
+                        {
+                            Console.WriteLine("Транзакция не проведена: " + newTransaction.Comment);
+                            break;
+                        }
+                        userInterface.AddTransaction(newTransaction);
                         break;
                     case 2:
-                        Bank.ShowTransactionHistory();
+                        ShowTrasactions(userInterface);
                         break;
                     case 3:
-                        Bank.ShowBalance();
+                        userInterface.CalculateAndShowBalance();
                         break;
                     case 4:
                         exit = true;
                         break;
                     case 5:
-                        Bank.SaveData();
-                        break;
-                    case 6:
                         Console.Clear();
                         break;
                     default:
                         Console.WriteLine("Некорректный выбор");
                         break;
                 }
+            }
+        }
+        public static Transaction NewTransaction()
+        {
+            Transaction newTransaction = new Transaction();
+            newTransaction.Date = DateTime.Now.ToUniversalTime();
+            try
+            {
+                Console.WriteLine("Введите сумму:");
+                newTransaction.Amount = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите категорию:");
+                newTransaction.Category = Console.ReadLine();
+
+                Console.WriteLine("Введите комментарий:");
+                newTransaction.Comment = Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                newTransaction.Amount = 0;
+                newTransaction.Category = "Error";
+                newTransaction.Comment = ex.Message;
+
+                
+            }
+            return newTransaction;
+
+        }
+
+        public static void ShowTrasactions(UserInterface userInterface)
+        {
+            List<Transaction> transactions = userInterface.ShowTransactionsForUser();
+
+            foreach (Transaction transaction in transactions)
+            {
+                Console.WriteLine($"ID: {transaction.id}| Date: {transaction.Date} | Sum: {transaction.Amount} | Category: {transaction.Category} |  Comment: {transaction.Comment}"); 
             }
         }
     }
--- Finanse/Class1.cs
+++ Finanse/Class1.cs
@@ -5,100 +5,119 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
 namespace Finanse
 {
     public class Transaction
     {
+        public int id {  get; set; }
         public DateTime Date { get; set; }
-        public decimal Sum { get; set; }
-
+        public decimal Amount { get; set; }
         public string Category { get; set; }
         public string Comment { get; set; }
-        public Guid id { get; set; }
     }
 
-    public class Bank
+    public class DatabaseManager
     {
-        static List<Transaction> transactions = new List<Transaction>();
+        private string connectionString;
 
-        public static void AddTransaction()
+        public DatabaseManager(string connectionString)
         {
-            Transaction newTransaction = new Transaction();
-
-            newTransaction.Date = DateTime.Now;
-
-            Console.WriteLine("Укажите сумму:");
-            newTransaction.Sum = decimal.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите категорию:");
-            newTransaction.Category = Console.ReadLine();
-
-            Console.WriteLine("Введите комментарий:");
-            newTransaction.Comment = Console.ReadLine();
-
-            newTransaction.id = Guid.NewGuid();
-
-            transactions.Add(newTransaction);
-            Console.WriteLine("Создана новая транзакция");
+            this.connectionString = connectionString;
         }
 
-        public static void ShowTransactionHistory()
+        public void AddTransaction(Transaction transaction)
         {
-            Console.WriteLine("История транзакций:");
+            string query = "INSERT INTO Bank (creation_date, summary, category, comment) " +
+                           "VALUES (@CreationDate, @Summary, @Category, @Comment)";
 
-            foreach (var transaction in transactions)
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
-                Console.WriteLine($"Дата: {transaction.Date} | Id: {transaction.id} | Сумма: {transaction.Sum} | Категория: {transaction.Category} | Комментарий: {transaction.Comment}");
+                command.Parameters.AddWithValue("@CreationDate", transaction.Date);
+                command.Parameters.AddWithValue("@Summary", transaction.Amount);
+                command.Parameters.AddWithValue("@Category", transaction.Category);
+                command.Parameters.AddWithValue("@Comment", transaction.Comment);
+
+                connection.Open();
+                command.ExecuteNonQuery();
             }
         }
 
-        public static void ShowBalance()
+        public decimal CalculateBalance()
         {
-            decimal Dohod = transactions.Where(t => t.Sum > 0).Sum(t => t.Sum);
-            decimal Rashod = transactions.Where(t => t.Sum < 0).Sum(t => t.Sum);
 
-            decimal balance = Dohod + Rashod;
-            Console.WriteLine($"Доходы: {Dohod} KZT\tРасход: {Rashod} KZT");
-            Console.WriteLine($"Общий баланс: {balance} KZT");
+            decimal balance = 0;
+            string query = "SELECT SUM(CASE WHEN summary > 0 THEN summary ELSE 0 END) - " +
+                           "SUM(CASE WHEN summary < 0 THEN summary ELSE 0 END) " +
+                           "FROM Bank";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            { 
+
+                connection.Open();
+                var result = command.ExecuteScalar();
+                if (result != DBNull.Value)
+                {
+                    balance = Convert.ToDecimal(result);
+                }
+            }
+
+            return balance;
         }
 
-        public static void LoadData()
-        {
-            Console.WriteLine("Укажите путь до файла (без расширения)");
-            string Path = Console.ReadLine() + ".txt";
-            if (File.Exists(@Path))
-            {
-                using (StreamReader sr = new StreamReader(Path))
+        public List<Transaction> ShowTransactionsByUserId()
+        { 
+            string query = "SELECT * FROM Bank";
+            List<Transaction> transactionList = new List<Transaction>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            { 
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
+                    while (reader.Read())
                     {
-                        string[] data = line.Split('|');
-                        Transaction transaction = new Transaction()
-                        {
-                            Date = DateTime.Parse(data[0]),
-                            id = Guid.Parse(data[1]),
-                            Sum = decimal.Parse(data[2]),
-                            Category = data[3],
-                            Comment = data[4]
-                        };
-                        transactions.Add(transaction);
+                        Transaction t = new Transaction();
+                        t.id = reader.GetInt32(0);
+                        t.Date = reader.GetDateTime(1);
+                        t.Amount = reader.GetDecimal(2);
+                        t.Category= reader.GetString(3);
+                        t.Comment = reader.GetString(4);
+                        
+                        transactionList.Add(t);
                     }
                 }
             }
+            return transactionList;
+        }
+    }
+    public class UserInterface
+    {
+        private DatabaseManager dbManager;
+
+        public UserInterface(string connectionString)
+        {
+            dbManager = new DatabaseManager(connectionString);
         }
 
-        public static void SaveData()
-        {
-            Console.WriteLine("Укажите путь файла (без расширения)");
-            string path = Console.ReadLine();
-            using (StreamWriter sw = new StreamWriter(path + ".txt"))
-            {
-                foreach (var transaction in transactions)
-                {
-                    sw.WriteLine($"{transaction.Date}|{transaction.id}|{transaction.Sum}|{transaction.Category}|{transaction.Comment}");
-                }
-            }
+        public List<Transaction> ShowTransactionsForUser()
+        { 
+            return dbManager.ShowTransactionsByUserId();
         }
+        public void AddTransaction(Transaction transaction)
+        {
+            dbManager.AddTransaction(transaction);
+        }
+
+        public void CalculateAndShowBalance()
+        {
+            decimal balance = dbManager.CalculateBalance();
+            Console.WriteLine(balance + " KZT");
+        }
+
     }
 }
